e7e3d1d73bdec3941c0579681a828263e4936377
lolalo200 <damianmtz644@gmail.com>
2025-12-13 20:52:22 -0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const app = initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "...",
+  appId: "..."
+};
